6923807889f4cffca08c2b6bc81c0f9dad1d001c
NetSture <netsture@gmail.com>
2025-12-14 19:00:52 +0530

    Advertisement media and image create

--- app/Http/Controllers/RouterstatusController.php
+++ app/Http/Controllers/RouterstatusController.php
@@ -27,6 +27,7 @@ class RouterstatusController extends Controller
             'event_date'  => $request->date ?? '2025-12-12',
             'event_time'  => $request->time ?? '12:00:00',
             'ip_address'  => $request->ip(),
+            'api_request'  => $request->all(),
         ]);
 
         // MikroTik expects plain response
